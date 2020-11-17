1114cd33c480825ce0d3ef446703a75e39945b3a
railstesh <railsriteshc56@gmail.com>
2020-11-17 22:14:21 +0530

    create food recipe model

---
+++ server/models/foodRecipeModel.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose")
+
+const Schema = mongoose.Schema
+
+const recipeSchema = new Schema({
+  recipeId: { type: String },
+  recipeName: { type: String },
+  recipeType: { type: String },
+  recipeAuthorName: { type: String },
+  recipeDescription: { type: String },
+  recipeIngredients: { type: Array },
+  recipeDirections: { type: Array },
+}, {
+  timestamps: [{ createdAt: 'created_at', updatedAt: 'updated_at' }],
+})
+
+module.exports = mongoose.model('Recipe', recipeSchema)
